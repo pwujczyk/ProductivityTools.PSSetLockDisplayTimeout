727456521d598215a2af38c503935293dfc1e123
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-25 18:07:13 +0200

    refactor psd1

--- ProductivityTools.PSSetLockDisplayTimeout/ProductivityTools.PSSetLockDisplayTimeout.psm1
+++ ProductivityTools.PSSetLockDisplayTimeout/ProductivityTools.PSSetLockDisplayTimeout.psm1
@@ -11,26 +11,9 @@ function Set-LockDisplayTimeout {
 	Write-Verbose "Hello from Set-LockDisplayTimeout"
 
 	AddOptionToPowerOptions
+	SetLockDisplayTimeout
 
-	$seconds=$Minutes*60;
-	$batterycommand="powercfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK " +$seconds
-	$pluggedInCommand="powercfg /SETACVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK "+ $seconds
-
-	if ($Battery.IsPresent)
-	{
-		Invoke-Expression -Command  $batterycommand;
-	}
-
-	if ($PluggedIn.IsPresent)
-	{
-		Invoke-Expression -Command $pluggedInCommand;
-	}
-
-	if($Battery.IsPresent -eq $false -and $PluggedIn.IsPresent -eq $false)
-	{
-		Invoke-Expression -Command $batterycommand;
-		Invoke-Expression -Command $pluggedInCommand;
-	}
+	Write-Verbose "Bye, Bye!"
 }
 
 function AddOptionToPowerOptions()
@@ -41,4 +24,40 @@ function AddOptionToPowerOptions()
 	Write-Verbose "Command which will be executed $command"
 	Invoke-Expression -Command $command
 	Write-Verbose "Options to power options should be added"
+}
+
+function SetLockDisplayTimeout()
+{
+	Write-Verbose "Set LockDisplayTimeout start"
+
+	$seconds=$Minutes*60;
+	$batterycommand="powercfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK " +$seconds
+	$pluggedInCommand="powercfg /SETACVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK "+ $seconds
+
+	if ($Battery.IsPresent)
+	{
+		Write-Verbose "Battery switch is present"
+		Write-Verbose "Invoke expression $batterycommand"
+		Invoke-Expression -Command  $batterycommand;
+	}
+
+	if ($PluggedIn.IsPresent)
+	{
+		Write-Verbose "PluggedIn switch is present"
+		Write-Verbose "Invoke expression $pluggedInCommand"
+		Invoke-Expression -Command $pluggedInCommand;
+	}
+
+	if($Battery.IsPresent -eq $false -and $PluggedIn.IsPresent -eq $false)
+	{
+		Write-Verbose "Battery switch is not present"
+		Write-Verbose "Invoke expression $batterycommand"
+		Write-Verbose "PluggedIn switch is present"
+		Write-Verbose "Invoke expression $pluggedInCommand"
+
+		Invoke-Expression -Command $batterycommand;
+		Invoke-Expression -Command $pluggedInCommand;
+	}
+
+	Write-Verbose "Set LockDisplayTimeout end"
 }
--- ProductivityTools.PSSetLockDisplayTimeout/Runner.ps1
+++ ProductivityTools.PSSetLockDisplayTimeout/Runner.ps1
@@ -1,4 +1,8 @@
 cd $PSScriptRoot
 
 Import-Module .\ProductivityTools.PSSetLockDisplayTimeout.psm1 -Force
-Set-LockDisplayTimeout -Minutes 11 -Verbose
+Set-LockDisplayTimeout -Minutes 1 -Verbose
+Write-Host "-------"
+Set-LockDisplayTimeout -Minutes 1 -PluggedIn -Verbose
+Write-Host "-------"
+Set-LockDisplayTimeout -Minutes 1 -Battery -Verbose
